c2190cd2b3f2f47fc392611be93d877905d96001
nala0897@naver.com <nala0897@naver.com>
2024-04-03 18:04:25 +0900

    first commit

---
+++ lib/birthday_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class MyHomeWork extends StatelessWidget {
+  const MyHomeWork({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Lottie.network(
+              'https://lottie.host/d166e944-e817-4dbd-95f2-17ef57958e59/bEN4xHnhnn.json'),
+          const Text(
+            'HAPPY BIRTHDAY',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            'MY SWEET 💗',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
