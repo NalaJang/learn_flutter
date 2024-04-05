490f89535e9035aa3e100551f89f5424b6ead0d3
nala0897@naver.com <nala0897@naver.com>
2024-04-05 17:33:03 +0900

    feat: what's new model class

---
+++ lib/240405/data/model/whats_new_model.dart
@@ -0,0 +1,11 @@
+class WhatsNewModel {
+  final String image;
+  final String title;
+  final String subtitle;
+
+  const WhatsNewModel({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+}
