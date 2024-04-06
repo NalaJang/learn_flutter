e9d1281f670b45cecc99f95e967cbfedd694ebd0
nala0897@naver.com <nala0897@naver.com>
2024-04-07 08:54:44 +0900

    feat: 웹 뷰 이동 제한

--- lib/web_view/web_view_screen.dart
+++ lib/web_view/web_view_screen.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
 // Import for Android features.
 import 'package:webview_flutter_android/webview_flutter_android.dart';
+
 // Import for iOS features.
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
@@ -22,7 +24,25 @@ class _WebViewScreenState extends State<WebViewScreen> {
     super.initState();
 
     controller = WebViewController()
-    ..loadRequest(Uri.parse('https://flutter.dev'));
+      ..loadRequest(
+        Uri.parse('https://flutter.dev'),
+      )
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (request) {
+          // 유튜브 링크로 들어가는 것을 막는다.
+          if( request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          // 그 외에는 이동을 허용
+          return NavigationDecision.navigate;
+        },
+        onUrlChange: (UrlChange change) {
+          debugPrint('url change to ${change.url}');
+        },
+        onPageFinished: (String url) {
+          debugPrint('page finished loading url: $url');
+        },
+      ));
   }
 
   @override
