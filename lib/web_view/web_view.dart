1cd774a357bf119ba04e9195cd8ea967fd8044d9
nala0897@naver.com <nala0897@naver.com>
2024-04-07 01:07:40 +0900

    feat: web view 예제 생성

---
+++ lib/web_view/web_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WebViewEx extends StatelessWidget {
+  const WebViewEx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('나만의 웹 브라우저'),
+      ),
+    );
+  }
+}
