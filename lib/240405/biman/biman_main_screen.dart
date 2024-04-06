d597ad04131d44c835a2fa4eaa6ac9a807ff5d33
nala0897@naver.com <nala0897@naver.com>
2024-04-06 16:24:05 +0900

    feat: textController 에 값 담아서 보내기

--- lib/240405/biman/biman_main_screen.dart
+++ lib/240405/biman/biman_main_screen.dart
@@ -11,6 +11,15 @@ class BimanMainScreen extends StatefulWidget {
 class _BimanMainScreenState extends State<BimanMainScreen> {
   // 이 키를 통해서 해당 form 의 상태를 알 수 있다.
   final _formKey = GlobalKey<FormState>();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +35,7 @@ class _BimanMainScreenState extends State<BimanMainScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               TextFormField(
+                controller: _heightController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '키',
@@ -40,6 +50,7 @@ class _BimanMainScreenState extends State<BimanMainScreen> {
               ),
               SizedBox(height: 16.0),
               TextFormField(
+                controller: _weightController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '몸무게',
@@ -56,15 +67,18 @@ class _BimanMainScreenState extends State<BimanMainScreen> {
               ElevatedButton(
                 onPressed: () {
                   // null 이 아닐 때는 validate(확인)을 하고, null 이면 false
-                  if (_formKey.currentState?.validate() ?? false) {
+                  if (_formKey.currentState?.validate() == false) {
                     return;
                   }
+
+                  final height = double.parse(_heightController.text);
+                  final weight =double.parse(_weightController.text);
 
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) =>
-                          ResultScreen(height: 0.0, weight: 0.0),
+                          ResultScreen(height: height, weight: weight),
                     ),
                   );
                 },
