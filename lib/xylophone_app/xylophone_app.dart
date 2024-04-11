be9d79510df02a11298e55300b3801803f14dd2c
nala0897@naver.com <nala0897@naver.com>
2024-04-11 19:15:58 +0900

    feat: 건반에 사운드 연결

--- lib/xylophone_app/xylophone_app.dart
+++ lib/xylophone_app/xylophone_app.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
   const XylophoneApp({super.key});
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  static Soundpool pool =
+  Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
+  bool _isLoading = true;
+  final List<int> _soundIds = [];
+  List<double> heightPaddings = [10, 20, 32, 40, 46, 54, 60, 69];
+  List<String> soundFileNames = ['do1', 're', 'mi', 'fa', 'sol', 'la', 'si', 'do2'];
+  List<String> syllableNames = ['도', '레', '미', '파', '솔', '라', '시', '도'];
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.purple,
+    Colors.cyan,
+    Colors.amberAccent,
+    Colors.blue,
+    Colors.deepOrange,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // initSoundPool();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    initSoundPool();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,57 +49,50 @@ class XylophoneApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text('실로폰'),
       ),
-      body: Row(
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: gunban('도', Colors.red),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: gunban('레', Colors.green),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: gunban('미', Colors.orange),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: gunban('파', Colors.deepOrangeAccent),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 46.0),
-            child: gunban('솔', Colors.purple),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 54.0),
-            child: gunban('라', Colors.amber),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 60.0),
-            child: gunban('시', Colors.blue),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 69.0),
-            child: gunban('도', Colors.lightBlueAccent),
-          ),
+          for (int i = 0; i < syllableNames.length; i++)
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: heightPaddings[i]),
+              child: gunban(syllableNames[i], colors[i], _soundIds[i]),
+            ),
         ],
       ),
     );
   }
 
-  Widget gunban(String text, Color color) {
-    return Container(
-      width: 50,
-      height: double.infinity,
-      color: color,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(color: Colors.white),
+  Widget gunban(String text, Color color, int soundId) {
+    return GestureDetector(
+      onTap: () {
+        pool.play(soundId);
+      },
+      child: Container(
+        width: 50,
+        height: double.infinity,
+        color: color,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> initSoundPool() async {
+    for (String fileName in soundFileNames) {
+      int soundId = await rootBundle
+          .load('assets/xylophone_sounds/$fileName.wav')
+          .then((soundData) => pool.load(soundData));
+      _soundIds.add(soundId);
+
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 }
