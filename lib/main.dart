4a3fb69a630e0bd12ad641cb306d235d49666752
nala0897@naver.com <nala0897@naver.com>
2024-04-14 23:50:11 +0900

    feat: Hive 사용 준비

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:learn_fluuter_together/240411/data/data_source/photo_data_source.dart';
 import 'package:learn_fluuter_together/240411/data/repository/photo_repo_impl.dart';
 import 'package:learn_fluuter_together/240411/presentation/search_list_screen.dart';
 import 'package:learn_fluuter_together/240411/presentation/search_list_view_model.dart';
 import 'package:learn_fluuter_together/gps/gps_map_app.dart';
+import 'package:learn_fluuter_together/todo_list/model/todo.dart';
 import 'package:learn_fluuter_together/todo_list/presentation/list_screen.dart';
 import 'package:learn_fluuter_together/xylophone_app/xylophone_app.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +16,17 @@ import 'sensor/sensor.dart';
 import 'stop_watch/stop_watch_screen.dart';
 import 'web_view/web_view_screen.dart';
 
-void main() {
+late Box<Todo> todos;
+
+void main() async {
+  // Hive 초기화
+  await Hive.initFlutter();
+  // pubspec.yaml 의 hive_generator, build_runner 를 통해 g.dart 파일을 만들고
+  // 그것들을 통해 TodoAdapter()를 만들 수 있다.
+  Hive.registerAdapter(TodoAdapter());
+  // todos(객체)를 통해서 DB 에 접근할 수 있다.
+  todos = await Hive.openBox<Todo>('todolist.db');
+
   runApp(const MyApp());
 }
 
--- lib/todo_list/model/todo.dart
+++ lib/todo_list/model/todo.dart
@@ -1,8 +1,20 @@
-class Todo {
+import 'package:hive/hive.dart';
+
+part 'todo.g.dart';
+
+// Hive DB 에 저장
+@HiveType(typeId: 0)
+class Todo extends HiveObject {
+  @HiveField(0)
+  int? id;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final int dateTime;
 
-  const Todo({
+  Todo({
     required this.title,
     required this.dateTime,
   });
